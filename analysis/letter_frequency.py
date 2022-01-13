5461475b5cf3cf320e532b54b22297fc5bf7da91
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-01-13 13:55:25 -0600

    Get nice looking results for letter frequency

--- analysis/letter_frequency.py
+++ analysis/letter_frequency.py
@@ -1,3 +1,5 @@
+## TODO: Convert this into a function that returns
+
 # Dictionary of letters and the amount of occurences for each in the database
 letters = {}
 
@@ -22,8 +24,38 @@ for word in database:
 database.close()
 
 # print the percentage of each letter
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 print("Total number of letters is: " + str(totalLetters))
+# sort the list from highest to lowest
+# https://www.tutorialsteacher.com/articles/sort-dict-by-value-in-python
+sortedLetters = sorted(letters.items(), key=lambda x:x[1], reverse=True)
 
-for char in alphabet:
-    print(char + ": " + str(letters[char] / totalLetters * 100) + "%")
+for item in sortedLetters:
+    print(item[0] + ": {:.2f}%".format(item[1] / totalLetters * 100))
+
+# Total number of letters is: 33780
+# E: 10.34%
+# S: 9.81%
+# A: 9.13%
+# R: 6.64%
+# O: 6.49%
+# I: 5.88%
+# L: 5.65%
+# T: 5.22%
+# N: 5.03%
+# D: 3.97%
+# U: 3.59%
+# C: 3.40%
+# Y: 3.00%
+# M: 2.98%
+# P: 2.95%
+# H: 2.81%
+# B: 2.55%
+# G: 2.40%
+# K: 2.02%
+# F: 1.73%
+# W: 1.59%
+# V: 1.17%
+# Z: 0.53%
+# X: 0.50%
+# J: 0.44%
+# Q: 0.18%
