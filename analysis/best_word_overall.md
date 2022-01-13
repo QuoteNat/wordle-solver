2229e9d37cfbdc7072c1ff33e8a346b8770dd668
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-01-13 14:21:57 -0600

    Create word ratings

---
+++ analysis/best_word_overall.md
@@ -0,0 +1,7 @@
+Score each word by adding up the tally value for each letter, but skip repeated letters.
+
+For example:
+TRAIN = valT + valR + valA + valI + valN
+TREES = valT + valR + valE + valS
+
+No idea if this is the best way to do this.
---
+++ analysis/best_word_overall.py
@@ -0,0 +1,26 @@
+from letter_frequency import getFrequency
+
+def getRating(word):
+    rating = 0
+    seenList = []
+    for letter in word:
+        if letter not in seenList:
+            rating += letterFrequency[letter]
+            seenList.append(letter)
+    return rating
+
+letterFrequency = getFrequency()
+
+database = open("../database/filtered.txt", "r")
+
+answerDict = {}
+
+for word in database:
+    word = word.strip()
+    answerDict[word] = getRating(word)
+
+sortedScores = sorted(answerDict.items(), key=lambda x:x[1], reverse=True)
+
+overallRating = open("./overallRating.txt", "w")
+for score in sortedScores:
+    overallRating.writelines(score[0] + ": " + str(score[1]) + "\n")
